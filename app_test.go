81f3b27bff3a6f05df85b26c7c1b87d24e9ce706
kassim <kassimali114@gmail.com>
2024-11-02 12:39:43 +0300

    correct password

--- app_test.go
+++ app_test.go
@@ -13,7 +13,7 @@ var a App
 
 func TestMain(m *testing.M) {
 
-	err := a.Initialize(Dbuser, DbPasswrod, "test")
+	err := a.Initialize(Dbuser, "kassim.TEstso@me34", "test")
 	if err != nil {
 		log.Fatal("an error occured while initilizing the database")
 	}
